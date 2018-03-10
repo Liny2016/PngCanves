036039a79d9a0f2ba775700b4620ce08b7b557e2
Liny2016 <782279031@qq.com>
2018-03-11 01:30:01 +0800

    update canvas.js..

--- js/canvas2image.js
+++ js/canvas2image.js
@@ -46,7 +46,7 @@ var Canvas2Image = function () {
 	function saveFile(data, filename,type){
 	    var save_link = document.createElementNS('http://www.w3.org/1999/xhtml', 'a');
 	    save_link.href = data;
-	    save_link.download = filename ;
+	    save_link.download = filename+'.png';
 	   
 	    var event = document.createEvent('MouseEvents');
 	    event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
